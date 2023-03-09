53101b43e6ee4136ef17ad3dda6bead22db7c5bb
tomare-moroizumi <m.ryousuke0401@icloud.com>
2023-03-10 04:52:06 +0900

    added logout button

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,21 +1,38 @@
-import { Burger, Header, MediaQuery } from '@mantine/core'
+import { supabase } from '@/libs/supabase'
+import useStore from '@/store'
+import { Burger, Button, Header, MediaQuery } from '@mantine/core'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
-const HeaderComponent = (opened: boolean, setOpened: any, theme: any) => (
-  <Header height={{ base: 50, md: 70 }} p="md">
-    <div style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
-      <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
-        <Burger
-          opened={opened}
-          onClick={() => setOpened((o: any) => !o)}
-          size="sm"
-          color={theme.colors.gray[6]}
-          mr="xl"
-        />
-      </MediaQuery>
-      <Link href={'/admin/dashboard'}>LOGO</Link>
-    </div>
-  </Header>
-)
+const HeaderComponent = (opened: boolean, setOpened: any, theme: any) => {
+
+  const router = useRouter()
+  const setSessionUser = useStore((state) => state.setSessionUser)
+  const logout = () => {
+    setSessionUser(null)
+    supabase.auth.signOut()
+    router.push('/')
+  }
+  
+  return (
+    <Header height={{ base: 50, md: 70 }} p="md">
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', height: '100%' }}>
+        <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
+          <Burger
+            opened={opened}
+            onClick={() => setOpened((o: any) => !o)}
+            size="sm"
+            color={theme.colors.gray[6]}
+            mr="xl"
+          />
+        </MediaQuery>
+        <Link href={'/admin/dashboard'}>LOGO</Link>
+        <Button color="pink" size="xs" onClick={logout}>
+          ログアウト
+        </Button>
+      </div>
+    </Header>
+  )
+}
 
 export default HeaderComponent
